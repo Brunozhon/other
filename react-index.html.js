162fa8cfe7e169a67bd09b8c423feb14aadf69a4
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-05-23 12:16:40 -0400

    Update react-index.html.js

--- react-index.html.js
+++ react-index.html.js
@@ -2,5 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom'; 
 
 class App extends React.Component {
-  
+  render() {
+    return (
+      <div>
+        <h1>Welcome to this page!</h1>
+      </div>
+    );
+  }
 }
