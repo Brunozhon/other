b551ae927a7ee6361c1847422a91b0b8aee45dd7
Bruno Zhong <69879040+Brunozhon@users.noreply.github.com>
2021-05-23 12:19:21 -0400

    Update react-index.html.js

--- react-index.html.js
+++ react-index.html.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom'; 
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div>
+      
         <h1>Welcome to this page!</h1>
+        <p>This is where I store my other stuff.</p>
       </div>
     );
   }
